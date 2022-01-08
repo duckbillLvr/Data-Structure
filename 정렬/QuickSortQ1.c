d0c6addc6917f543b509e58dd49bb9b41ba0f468
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-01-08 22:03:38 +0900

    퀵 정렬

--- 정렬/QuickSortQ1.c
+++ 정렬/QuickSortQ1.c
@@ -7,11 +7,9 @@ typedef int Data;
 void Swap(Data *arr, int idx1, int idx2);
 int Partition(Data *arr, int left, int right);
 void QuickSort(Data *arr, int left, int right);
-
+int MedianOfThree(int *arr, int left, int right);
 int main()
 {
-    // Data arr[7] = {3, 2, 4, 1, 7, 6, 5};
-    // Data arr[3] = {3, 3, 3};
     Data arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
     int len = sizeof(arr) / sizeof(Data);
@@ -28,9 +26,14 @@ void Swap(Data *arr, int idx1, int idx2)
 }
 int Partition(Data *arr, int left, int right)
 {
-    int pivot = arr[left]; //피벗은 가장 왼쪽에 위치
+    // int pivot = arr[left]; //피벗은 가장 왼쪽에 위치
+    int pIdx = MedianOfThree(arr, left, right);
+    int pivot = arr[pIdx];
+
     int low = left + 1;
     int high = right;
+
+    Swap(arr, pIdx, left);
 
     printf("pivot: %d\n", pivot); //피벗 확인을 위함
     while (low <= high)           //교차 되지 않을 때까지 반복
@@ -49,10 +52,24 @@ int Partition(Data *arr, int left, int right)
 }
 void QuickSort(Data *arr, int left, int right)
 {
-    if (left <= right)
+    if (left < right)
     {
         int pivot = Partition(arr, left, right);
         QuickSort(arr, left, pivot - 1);
         QuickSort(arr, pivot + 1, right);
     }
+}
+int MedianOfThree(int *arr, int left, int right)
+{
+    int samples[3] = {left, (left + right) / 2, right};
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2 - i; j++)
+        {
+            if (arr[samples[j]] > arr[samples[j]])
+                Swap(samples, j, j + 1);
+        }
+    }
+
+    return samples[1];
 }
