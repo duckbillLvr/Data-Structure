883b565c70c22ff35fe6c7df12d208013e566986
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-02-05 19:58:21 +0900

    이진트리의 순회

--- 트리/BinaryTreeTraverse.c
+++ 트리/BinaryTreeTraverse.c
@@ -28,7 +28,7 @@ void InorderTraverse(BTreeNode *bt)
         return;
 
     InorderTraverse(bt->left);
-    printf("%d\n", bt->data);
+    printf("%d ", bt->data);
     InorderTraverse(bt->right);
 }
 // 2. 전위 순위 함수
@@ -37,7 +37,7 @@ void PreorderTraverse(BTreeNode *bt)
     if (bt == NULL)
         return;
 
-    printf("%d\n", bt->data);
+    printf("%d ", bt->data);
     PreorderTraverse(bt->left);
     PreorderTraverse(bt->right);
 }
@@ -49,7 +49,7 @@ void PostorderTraverse(BTreeNode *bt)
 
     PostorderTraverse(bt->left);
     PostorderTraverse(bt->right);
-    printf("%d\n", bt->data);
+    printf("%d ", bt->data);
 }
 int main()
 {
@@ -58,17 +58,28 @@ int main()
     BTreeNode *bt3 = MakeBTreeNode();
     BTreeNode *bt4 = MakeBTreeNode();
     BTreeNode *bt5 = MakeBTreeNode();
+    BTreeNode *bt6 = MakeBTreeNode();
+    BTreeNode *bt7 = MakeBTreeNode();
     SetData(bt1, 1);
     SetData(bt2, 2);
     SetData(bt3, 3);
     SetData(bt4, 4);
     SetData(bt5, 5);
+    SetData(bt6, 6);
+    SetData(bt7, 7);
     MakeLeftSubTree(bt1, bt2);
     MakeRightSubTree(bt1, bt3);
     MakeLeftSubTree(bt2, bt4);
     MakeRightSubTree(bt2, bt5);
+    MakeLeftSubTree(bt3, bt6);
+    MakeRightSubTree(bt3, bt7);
 
+    PreorderTraverse(bt1);
+    printf("\n");
     InorderTraverse(bt1);
+    printf("\n");
+    PostorderTraverse(bt1);
+
     return 0;
 }
 BTreeNode *MakeBTreeNode(void) //노드의 생성
